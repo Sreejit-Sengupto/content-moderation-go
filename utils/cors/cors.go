58dac153087208732360f9552dd68cc31d4e28a1
Sreejit-Sengupto <sreesen2003@gmail.com>
2026-01-27 11:37:50 +0530

    feat: Add localhost:3000 and render.com to allowed CORS origins.

--- utils/cors/cors.go
+++ utils/cors/cors.go
@@ -9,6 +9,8 @@ func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Allow requests from the Next.js frontend
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "https://content-moderation-go.onrender.com/")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
