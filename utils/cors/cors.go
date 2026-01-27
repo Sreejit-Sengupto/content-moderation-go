2bf0cc2e5faecaf543120e1758f4cafeaa4ed1a6
Sreejit-Sengupto <sreesen2003@gmail.com>
2026-01-27 11:47:41 +0530

    feat: Dynamically set `Access-Control-Allow-Origin` based on a predefined list of allowed origins.

--- utils/cors/cors.go
+++ utils/cors/cors.go
@@ -4,13 +4,21 @@ import (
 	"net/http"
 )
 
+// Allowed origins for CORS
+var allowedOrigins = map[string]bool{
+	"http://localhost:3000": true,
+	"http://localhost:3001": true,
+}
+
 // Middleware adds CORS headers to allow requests from the Next.js frontend
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Allow requests from the Next.js frontend
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001")
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Origin", "https://content-moderation-go.onrender.com/")
+		origin := r.Header.Get("Origin")
+
+		// Check if the origin is allowed
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
